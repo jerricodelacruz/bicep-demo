2773f5675b4c3884283d90c35aed3697de606fa2
Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>
2023-01-25 17:27:25 +0800

    init
    Signed-off-by: Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>

--- goapp/main.go
+++ goapp/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "Hello world!!")
+        fmt.Fprint(w, "Hello world!")
     })
 
     http.ListenAndServe(":8080", nil)
